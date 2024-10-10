9199afaf5079489a4cddf9d7c3652ae4a35409fb
obidyhasan <obidyhasan@gmail.com>
2024-10-10 12:25:47 +0600

    get & show foods

--- js/script.js
+++ js/script.js
@@ -28,5 +28,46 @@ const showCategory = (categories) => {
   });
 };
 
+// Get Foods from API
+const getFoods = async (search = "") => {
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    );
+    const data = await res.json();
+    showFoods(data.meals);
+  } catch (error) {
+    console.log("Something went wrong in getFoods()");
+  }
+};
+
+// Show Foods from getFoods
+const foodContainer = document.querySelector("#foodContainer");
+const showFoods = (foods) => {
+  foods.forEach((food) => {
+    const { strMeal, strMealThumb, strInstructions } = food;
+    const item = document.createElement("div");
+    item.className = "p-4 border rounded-md space-y-3";
+    item.innerHTML = `
+        <figure class="w-full h-[200px]">
+            <img
+            src="${strMealThumb}"
+            class="w-full h-full object-cover rounded-lg"
+            alt=""
+            />
+        </figure>
+        <h2 class="text-xl font-semibold">${strMeal}</h2>
+        <p
+            class="line-clamp-2 text-sm text-gray-500"
+        >
+        ${strInstructions}
+        </p>
+        <button class="btn bg-primary">Show Details</button>
+    `;
+    foodContainer.appendChild(item);
+  });
+};
+
 // Function Calls
 getCategory();
+getFoods();
